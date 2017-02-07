dd7de6a89b93289ac750bdd1081839ac5ec8dd69
Alex Pavy <alex.g.pavy@gmail.com>
2017-02-07 01:25:55 +0100

    cannot set socket io for heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var router = require("./backend/router");
 var notificationService = require("./backend/notificationService");
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 
 app.listen(port, function () {
     console.log('Text convert machine backend listening on port '+ port +'!')
